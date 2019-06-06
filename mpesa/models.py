9c52831c98a7f1460327c03425d81fe910de0b52
Jaccojwang <danolago@gmail.com>
2019-06-06 09:49:55 +0300

    Create profile and statement models Make initial migration

--- mpesa/models.py
+++ mpesa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profilepic = models.ImageField(upload_to='profiles/')
+    prefname = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.prefname
+
+class Statement(models.Model):
+    month = models.CharField(max_length=15)
+    statement = models.FileField(upload_to='statements/')
+
+    def __str__(self):
+        return self.month
+
